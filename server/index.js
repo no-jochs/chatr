a122f34ace2cf7e9ff1adf2a09b5e15775aafc50
John Ochs <code@johnochs.io>
2022-12-21 13:55:54 -0800

    Began working to implement Socket.io server-side work.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const session = require('express-session');
+
 const path = require('path');
 const socketio = require('socket.io');
 const http = require('http');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    const startTime = new Date();
-    console.log(`${startTime.toLocaleTimeString()} | Express server running on port ${PORT}.`);
+const app = express();
+const httpServer = http.createServer(app);
+
+const sessionMiddleware = session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
 });
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(sessionMiddleware);
+
+app.post('/login', (req, res) => {
+    req.session.authenticated = true,
+        req.status(204).end();
+});
+
+const io = socketio(httpServer);
+
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+
+io.use(wrap(sessionMiddleware));
+// app.listen(PORT, () => {
+//     const startTime = new Date();
+//     console.log(`${startTime.toLocaleTimeString()} | Express server running on port ${PORT}.`);
+// });
+
+app.use(express.static(path.join(__dirname, '../dist/index.html')));
